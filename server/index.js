286e2208858024579b130ff143093bf0e445c97b
Ngo Tung Nam <namngoa4k16@gmail.com>
2026-08-18 17:29:55 +0700

    Fix CORS

--- server/index.js
+++ server/index.js
@@ -27,8 +27,10 @@ app.use(cors({
         }
     },
     credentials: true
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
 }));
-
+app.options('*', cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../public')));
 
